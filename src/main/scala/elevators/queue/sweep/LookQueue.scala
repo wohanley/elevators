f9fbf6c9c75c162d9a75d93bc57aaba5a44e226d
William O'Hanley <liam@wohanley.com>
2014-09-11 13:06:38 +1200

    head on look queue

--- src/main/scala/elevators/queue/sweep/LookQueue.scala
+++ src/main/scala/elevators/queue/sweep/LookQueue.scala
@@ -6,32 +6,33 @@ import elevators.queue.RequestQueue
 case class LookQueue(position: Int, requests: List[Int],
   direction: SeekDirection) extends RequestQueue[Int] {
 
+  private val compare = eligibleRequestFilter(position, direction)
+  
+  private val eligibleRequests = requests.filter(req => compare(req))
+  
+  private val service = if (eligibleRequests.nonEmpty) {
+    eligibleRequests.sortBy(difference(position)_).head
+  } else direction match {
+    /*
+     * If I'm looking up and there are no requests higher than my current
+     * position, I need to turn around and head down, and the first request
+     * I meet in that direction must be the largest in the list.
+     */
+    case Up => requests.max
+    // converse logic here
+    case Down => requests.min
+  }
+  
   override def enqueue(request: Int): LookQueue = {
     return new LookQueue(position, request :: requests, direction)
   }
   
   override def dequeue: (Int, LookQueue) = {
-    
-    val compare = eligibleRequestFilter(position, direction)
-    
-    val eligibleRequests = requests.filter(req => compare(req))
-      
-    val service = if (eligibleRequests.nonEmpty) {
-      eligibleRequests.sortBy(difference(position)_).head
-    } else direction match {
-      /*
-       * If I'm looking up and there are no requests higher than my current
-       * position, I need to turn around and head down, and the first request
-       * I meet in that direction must be the largest in the list.
-       */
-      case Up => requests.max
-      // converse logic here
-      case Down => requests.min
-    }
-        
-    return (service, new LookQueue(service,
-      requests.filter(req => req != service), newDirection(service)))
+    return (this.service, new LookQueue(this.service,
+      requests.filter(req => req != this.service), newDirection(this.service)))
   }
+
+  override def head: Int = this.service
   
   def newDirection(service: Int): SeekDirection =
     direction match {
