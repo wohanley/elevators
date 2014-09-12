ee2f8a98db06976ebe64115c33bec2f798249faa
William O'Hanley <liam@wohanley.com>
2014-09-12 14:27:21 +1200

    Revert "capture output in gifs"
    This reverts commit 1eface8bb63e803c79da56db256196505f0f8d4c.
    
    this is annoying as hell most of the time, but I didn't want to lose the code

--- src/main/scala/elevators/ui/MainPanel.scala
+++ src/main/scala/elevators/ui/MainPanel.scala
@@ -3,7 +3,6 @@ package elevators.ui
 import elevators._
 import elevators.queue.RequestQueue
 import elevators.util.first
-import elevators.util.GifExporter
 import java.awt.Container
 import java.awt.Dimension
 import java.awt.Graphics
@@ -35,8 +34,6 @@ class MainPanel(initialRequests: RequestQueue[Int]) extends JPanel with View {
     this.requestRegions += i -> region
   }
 
-  val giffer = new GifExporter()
-
   def enqueue(request: Int): Unit = {
     this.drawRequest(request)
     this.requests = this.requests.enqueue(request)
@@ -62,7 +59,6 @@ class MainPanel(initialRequests: RequestQueue[Int]) extends JPanel with View {
     this.requestRegions.get(request).map(region => {
       val result = f(region)
       region.repaint()
-      giffer.gif(this)
       return Some(result)
     })
   }
